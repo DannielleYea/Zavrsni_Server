81342214d53f82f58eeab672e5768aa0a9824292
danijel.turic@gmail.com <xKji27rC>
2018-07-05 23:19:26 +0200

    Added login and register http handlers

--- hello.go
+++ hello.go
@@ -4,12 +4,22 @@ import ("fmt"
 	"net/http"
 )
 
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Login data")
+}
+
+func register(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Register data")
+}
+
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
 	})
 
+	http.HandleFunc("/login", login);
+	
 	http.ListenAndServe(":80", nil)
 	
 	fmt.Println("Hello world!")
